c012138f1c5900a68f471c9c4d27e975f8d4ee4d
LayanBS7 <Layanbsa@hotmail.com>
2025-09-18 21:30:00 +0300

    update the theme - reem

--- lib/theme/base_themes/colors.dart
+++ lib/theme/base_themes/colors.dart
@@ -1,44 +1,47 @@
 import 'package:flutter/material.dart';
 
 class BColors {
- BColors._();
- ///App Basic Colors
- static const Color primary = Color(0xF5F8C85);
- static const Color secondry = Color(0xFC1DFC9);
- static const Color accent = Color(0xFE9B8A9); ///more brightness and saturation
+  BColors._();
 
- ///Text Colors
- static const Color texBlack = Color(0xFF000000);
- static const Color textwhite = Color(0xFFFFFF);
- static const Color textprimary= Color(0xF407067);
+  ///App Basic Colors
+  static const Color primary = Color(0xFF5F8C85);
+  static const Color secondry = Color(0xFFC1DFC9);
+  static const Color accent = Color(0xFFE9B8A9);
 
- ///Background Colors
- static const Color light = Color(0xFFF6F6F6);
+  ///more brightness and saturation
 
- ///Background Containers Colors
- static const Color lightContainer = Color(0xFFF6F6F6);
+  ///Text Colors
+  static const Color texBlack = Color(0xFF000000);
+  static const Color textwhite = Color(0xFFFFFFFF);
+  static const Color textprimary = Color(0xFF407067);
 
- ///Button Colors
- static const Color buttonPrimary = Color(0xF5F8C85);
- static const Color buttonSecondary = Color(0xFC1DFC9);
- static const Color buttonDisabled = Color(0xFFC4C4C4);
+  ///Background Colors
+  static const Color light = Color(0xFFF6F6F6);
 
- ///Border Colors
- static const Color borderPrimary = Color(0xFFD9D9D9);
- static const Color borderSecondary = Color(0xFFE6E6E6);
+  ///Background Containers Colors
+  static const Color lightContainer = Color(0xFFF6F6F6);
 
- ///Error and Validation Colors
- static const Color error = Color(0xFFc62828);
- static const Color warning = Color(0xFFe65100);
- static const Color success = Color(0xFF1b5e20);
- static const Color info = Color(0xFF01579b);
+  ///Button Colors
+  static const Color buttonPrimary = Color(0xFF5F8C85);
+  static const Color buttonSecondary = Color(0xFFC1DFC9);
+  static const Color buttonDisabled = Color(0xFFC4C4C4);
 
- ///Neutral Shades
- static const Color black = Color(0xFF272727);
- static const Color darkGrey = Color(0xFF939393);
- static const Color darkerGrey = Color(0xFF4F4F4F);
- static const Color grey = Color(0xFFE0E0E0);
- static const Color softGrey = Color(0xFFF4F4F4);
- static const Color lightGrey = Color(0xFFF9F9F9);
- static const Color white = Color(0xFFFFFFFF);
+  ///Border Colors
+  static const Color borderPrimary = Color(0xFFD9D9D9);
+  static const Color borderSecondary = Color(0xFFE6E6E6);
+
+  ///Error and Validation Colors
+  static const Color error = Color(0xFFc62828);
+  static const Color warning = Color(0xFFe65100);
+  static const Color success = Color(0xFF1b5e20);
+  static const Color info = Color(0xFF01579b);
+
+  ///Neutral Shades
+  static const Color black = Color(0xFF272727);
+  static const Color darkGrey = Color(0xFF939393);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color softGrey = Color(0xFFF4F4F4);
+  static const Color lightGrey = Color(0xFFF9F9F9);
+  static const Color white = Color(0xFFFFFFFF);
 }
