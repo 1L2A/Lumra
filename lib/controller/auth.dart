8983d9a0e552a34ab5688f11f8095f2989a322ef
LayanBS7 <Layanbsa@hotmail.com>
2025-09-19 13:18:19 +0300

    Update auth.dart

--- lib/controller/auth.dart
+++ lib/controller/auth.dart
@@ -1,1 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
 
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.message);
+    }
+  }
+
+  Future<void> resetPassword(String email) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.message);
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      throw Exception("Error signing out: $e");
+    }
+  }
+}
