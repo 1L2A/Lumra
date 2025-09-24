3e03f8b70645d60fb10eb1edba04e2af655da681
re1k <reem.kd78@gmail.com>
2025-09-24 11:24:03 +0300

    d

--- lib/view/HomePage/Calendar/eventWidgets/addEventView.dart
+++ lib/view/HomePage/Calendar/eventWidgets/addEventView.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lumra_project/controller/Homepage/Calendar/addEventController.dart';
+import 'package:lumra_project/controller/auth/auth_controller.dart';
 import 'package:lumra_project/theme/base_themes/colors.dart';
 import 'package:lumra_project/theme/base_themes/sizes.dart';
 import 'package:lumra_project/theme/custom_themes/text_theme.dart';
@@ -11,13 +12,15 @@ import 'package:intl/intl.dart';
 class AddEventView extends StatelessWidget {
   
   AddEventView({super.key});
+  final authContoller = Get.find<AuthController>();
 
   // Directly get the controller from GetX
   final AddEventController controller = Get.put(
     AddEventController(FirebaseFirestore.instance, FirebaseAuth.instance.currentUser!.uid),
   );
 
-  
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
